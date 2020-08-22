SanczoPL/calibre

Slight dulling of text brigthness in dark mode

src/calibre/gui2/palette.py
@@ -13,7 +13,7 @@ def dark_palette():
     p = QPalette()
     dark_color = QColor(45,45,45)
     disabled_color = QColor(127,127,127)
-    text_color = QColor('#eee')
+    text_color = QColor('#ddd')
     p.setColor(p.Window, dark_color)
     p.setColor(p.WindowText, text_color)
     p.setColor(p.Base, QColor(18,18,18))

clown-car/calibre-2

src/calibre/gui2/palette.py
@@ -8,7 +8,7 @@ from calibre.constants import dark_link_color
 
 dark_link_color = QColor(dark_link_color)
 dark_color = QColor(45,45,45)
-dark_text_color = QColor('#eee')
+dark_text_color = QColor('#ddd')
 
 
 def dark_palette():