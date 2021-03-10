clown-car/calibre-2

commit ea3fb4e406eeccb5c0cc57ef564aa3e8a8ad563c

1) Fix regression in template language: at some point in V5 the template language was changed to disallow using an expression_list as a parameter. This was allowed in previous major revisions. 2) Allow expression lists in parameterized expressions. 3) Correct the grammar in the documentation.

src/calibre/utils/formatter.py
@@ -568,7 +568,7 @@ class _Parser(object):
     def expr(self):
         if self.token_op_is_lparen():
             self.consume()
-            rv = self.top_expr()
+            rv = self.expression_list()
             if not self.token_op_is_rparen():
                 self.error(_('Missing )'))
             self.consume()
@@ -598,7 +598,7 @@ class _Parser(object):
             arguments = list()
             while not self.token_op_is_rparen():
                 # evaluate the expression (recursive call)
-                arguments.append(self.top_expr())
+                arguments.append(self.expression_list())
                 if not self.token_op_is_comma():
                     break
                 self.consume()
@@ -906,6 +906,8 @@ class _Interpreter(object):
 
     def expr(self, prog):
         try:
+            if isinstance(prog, list):
+                return self.expression_list(prog)
             return self.NODE_OPS[prog.node_type](self, prog)
         except ValueError as e:
             raise e

commit b5049cea0bdd0db6108066a2c1d4ef285d436519

Fix regression in template language caused by commit ea3fb4e: expression lists. The commit broke the arguments(), globals(), and set_globals() functions.

src/calibre/utils/formatter.py
@@ -616,7 +616,8 @@ class _Parser(object):
                 return AssignNode(arguments[0].name, arguments[1])
             if id_ == 'arguments' or id_ == 'globals' or id_ == 'set_globals':
                 new_args = []
-                for arg in arguments:
+                for arg_list in arguments:
+                    arg = arg_list[0]
                     if arg.node_type not in (Node.NODE_ASSIGN, Node.NODE_RVALUE):
                         self.error(_("Parameters to '{}' must be "
                                      "variables or assignments").format(id_))