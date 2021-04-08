t-8ch/calibre

load plugins from system-wide directory
https://www.mobileread.com/forums/showthread.php?t=333893

src/calibre/constants.py
@@ -166,6 +166,8 @@ def cache_dir():
 
 # plugins {{{
 plugins_loc = sys.extensions_location
+site_plugins_loc = getattr(sys, 'site_plugins_location', None)
+
 from importlib.machinery import ModuleSpec, EXTENSION_SUFFIXES, ExtensionFileLoader
 from importlib.util import find_spec
 from importlib import import_module

clown-car/calibre-2

src/calibre/constants.py
@@ -166,6 +166,8 @@ def cache_dir():
 
 # plugins {{{
 plugins_loc = sys.extensions_location
+system_plugins_loc = getattr(sys, 'system_plugins_location', None)
+
 from importlib.machinery import ModuleSpec, EXTENSION_SUFFIXES, ExtensionFileLoader
 from importlib.util import find_spec
 from importlib import import_module