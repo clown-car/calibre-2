10197add32ba3f2a62e7a6bae40f86dfbaa28e3c
chiph <chiph@github.com>
2021-05-21 07:28:38 -0700

    Revert "Test"

--- resources/csscolorparser.js
+++ resources/csscolorparser.js
@@ -20,8 +20,6 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 // IN THE SOFTWARE.
 
-// Comment
-
 // http://www.w3.org/TR/css3-color/
 var kCSSColorTable = {
   "transparent": [0,0,0,0], "aliceblue": [240,248,255,1],
